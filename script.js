d43d07340ab30a8573586822a630ec1d5fe85959
w-vinhatto <vvinhatto@hotmail.com>
2023-10-22 19:56:03 -0300

    comit-ajuste-resultado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const testResult = document.querySelector("#result")
 const testponctuationHuman = document.querySelector("#ponctuation-Human")
 const testponctuationMachine = document.querySelector("#ponctuation-Machine")
+const resultText = document.querySelector("#result-text")
 let ponctuationHuman = 0
 let ponctuationMachine = 0
 
@@ -19,16 +20,19 @@ const playTheGame = (player, machines) => {
 
     if (player === machines) {
         testResult.innerHTML = "&#x1F610;"
+        resultText.innerHTML = "Deu empate"
     } else if (
         player === 'pedra' && machines === 'tesoura' || 
         player === 'papel' && machines === 'pedra' || 
         player === 'tesoura' && machines === 'papel') {
             ponctuationHuman++
             testResult.innerHTML = "&#x1F60D;"
+            resultText.innerHTML = "Prabéns você venceu"
             testponctuationHuman.innerHTML = ponctuationHuman
 
     } else {
         testResult.innerHTML = "&#x1F622;"
+        resultText.innerHTML = "Você perdeu para alexa"
         ponctuationMachine++
         testponctuationMachine.innerHTML = ponctuationMachine
     }
