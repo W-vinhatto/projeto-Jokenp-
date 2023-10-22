925b74321c72a63570bf7d7d6c6a70183bcde0a8
w-vinhatto <vvinhatto@hotmail.com>
2023-10-22 20:19:46 -0300

    comit-p-enum

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const testponctuationHuman = document.querySelector("#ponctuation-Human")
 const testponctuationMachine = document.querySelector("#ponctuation-Machine")
 const resultText = document.querySelector("#result-text")
 let ponctuationHuman = 0
-let ponctuationMachine = 0
+let ponctuationMachine = 0   // camel case
+const GAME_OPTIONS = {       // snake case
+    STONE : "pedra",
+    PAPER: "papel",
+    SCISSORS: "tesoura"
+}
 
 function player(play) {
     playTheGame(play, machine())
@@ -11,7 +16,7 @@ function player(play) {
 
 
 const machine = () => {
-    const choice = ['pedra', 'papel', 'tesoura']
+    const choice = [GAME_OPTIONS.STONE,GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const nubRandon = Math.floor(Math.random() * 3)
     return choice[nubRandon]
 }
@@ -22,9 +27,9 @@ const playTheGame = (player, machines) => {
         testResult.innerHTML = "&#x1F610;"
         resultText.innerHTML = "Deu empate"
     } else if (
-        player === 'pedra' && machines === 'tesoura' || 
-        player === 'papel' && machines === 'pedra' || 
-        player === 'tesoura' && machines === 'papel') {
+        player === GAME_OPTIONS.STONE && machines === GAME_OPTIONS.SCISSORS || 
+        player === GAME_OPTIONS.PAPER && machines === GAME_OPTIONS.STONE || 
+        player === GAME_OPTIONS.SCISSORS && machines === GAME_OPTIONS.PAPER) {
             ponctuationHuman++
             testResult.innerHTML = "&#x1F60D;"
             resultText.innerHTML = "Prabéns você venceu"
